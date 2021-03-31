1fac86d117c5e8dfe1f81c391620fb9c09c48d5c
fliszt2 <rkelly2012@gmail.com>
2021-03-31 12:40:49 -0400

    fleshing out css grid

--- client/src/components/WinnersList.jsx
+++ client/src/components/WinnersList.jsx
@@ -23,14 +23,22 @@ class WinnersList extends React.Component {
     } else {
       return (
         <div className='winners-list'>
-          <h3>Best Picture</h3>
-          <Winner category='picture' displayName={this.state.winners.picture.Nominee} title={this.state.winners.picture.Nominee} review={this.state.winners.review}/>
-          <h3>Best Director</h3>
-          <Winner category='director' displayName={this.state.winners.director['Additional Info']} title={this.state.winners.director.Nominee}/>
-          <h3>Best Actress</h3>
-          <Winner category='actress' displayName={this.state.winners.actress.Nominee} title={this.state.winners.actress['Additional Info'].slice(0, this.state.winners.actress['Additional Info'].indexOf('{') - 1)}/>
-          <h3>Best Actor</h3>
-          <Winner category='actor' displayName={this.state.winners.actor.Nominee} title={this.state.winners.actor['Additional Info'].slice(0, this.state.winners.actor['Additional Info'].indexOf('{') - 1)}/>
+          <div className='picture'>
+            <h3>Best Picture</h3>
+            <Winner category='picture' displayName={this.state.winners.picture.Nominee} title={this.state.winners.picture.Nominee} review={this.state.winners.review} />
+          </div>
+          <div className='director'>
+            <h3>Best Director</h3>
+            <Winner category='director' displayName={this.state.winners.director['Additional Info']} title={this.state.winners.director.Nominee} />
+          </div>
+          <div className='actress'>
+            <h3>Best Actress</h3>
+            <Winner category='actress' displayName={this.state.winners.actress.Nominee} title={this.state.winners.actress['Additional Info'].slice(0, this.state.winners.actress['Additional Info'].indexOf('{') - 1)} />
+          </div>
+          <div className='actor'>
+            <h3>Best Actor</h3>
+            <Winner category='actor' displayName={this.state.winners.actor.Nominee} title={this.state.winners.actor['Additional Info'].slice(0, this.state.winners.actor['Additional Info'].indexOf('{') - 1)} />
+          </div>
         </div>
       );
     }

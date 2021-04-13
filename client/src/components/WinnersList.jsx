3995a35217f5bd42513d6a4612578b7cc91e5e61
fliszt2 <rkelly2012@gmail.com>
2021-04-13 12:37:00 -0400

    got rid of else statement in WinnersList

--- client/src/components/WinnersList.jsx
+++ client/src/components/WinnersList.jsx
@@ -26,18 +26,16 @@ class WinnersList extends React.Component {
           <img className='placeholder' id='oscar' src='../img/oscar.jpg'></img>
         </div>
       );
-    } else {
-      return (
-        <div className='container-winners'>
-          <Winner year={this.state.year} category='picture' displayName={this.state.winners.picture.Nominee} title={this.state.winners.picture.Nominee} review={this.state.winners.review} />
-          <Winner year={this.state.year} category='director' displayName={this.state.winners.director['Additional Info']} title={this.state.winners.director.Nominee} />
-          <Winner year={this.state.year} category='actress' displayName={this.state.winners.actress.Nominee} title={this.state.winners.actress['Additional Info'].slice(0, this.state.winners.actress['Additional Info'].indexOf('{') - 1)} />
-          <Winner year={this.state.year} category='actor' displayName={this.state.winners.actor.Nominee} title={this.state.winners.actor['Additional Info'].slice(0, this.state.winners.actor['Additional Info'].indexOf('{') - 1)} />
-        </div>
-      );
     }
+    return (
+      <div className='container-winners'>
+        <Winner year={this.state.year} category='picture' displayName={this.state.winners.picture.Nominee} title={this.state.winners.picture.Nominee} review={this.state.winners.review} />
+        <Winner year={this.state.year} category='director' displayName={this.state.winners.director['Additional Info']} title={this.state.winners.director.Nominee} />
+        <Winner year={this.state.year} category='actress' displayName={this.state.winners.actress.Nominee} title={this.state.winners.actress['Additional Info'].slice(0, this.state.winners.actress['Additional Info'].indexOf('{') - 1)} />
+        <Winner year={this.state.year} category='actor' displayName={this.state.winners.actor.Nominee} title={this.state.winners.actor['Additional Info'].slice(0, this.state.winners.actor['Additional Info'].indexOf('{') - 1)} />
+      </div>
+    );
   }
-
 }
 
 export default WinnersList;
